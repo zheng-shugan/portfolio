83a0543f040613d425ff0e98cb0c780186a8f985
zhengshugan <zhengshugan@foxmail.com>
2022-11-04 22:49:08 +0800

    fix: remove .DS_Store file

--- components/date.js
+++ components/date.js
@@ -3,5 +3,5 @@ import { parseISO, format } from "date-fns";
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
 
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  return <time dateTime={dateString}>{format(date, 'yyyy-MM-dd')}</time>;
 }
